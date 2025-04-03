13b48405193f190baf7aa930b96bb1f0a6896723
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2025-04-03 16:57:49 -0300

    feat: spl metadata and transfer

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("4Hn9Xr1Tvc9cuZRsWA3K3KP7izqSyB6SMaUX8QmTntxa");
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "Matheus",
+            symbol: "MMS",
+            uri: "https://www.matheus.com",
+            sellerFeeBasisPoints: 500,
+            creators: null,
+            collection: null,
+            uses: null,
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data: data,
+            isMutable: true,
+            collectionDetails: null,
+        }
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
+
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,11 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../../ts/cluster1/wallet/id.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,19 +16,44 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("Hej4tQLbH9QeLdRkH4VZQJGHAoPjB94QUo7GYWTao9jL");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("8T9GcCqpjy3NJX1nMkhVPhzJW8zgFJhNxASn4whhRTX2");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    const ata_from = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+    const ata_to = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to
+    );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    const tx = await transfer(
+      connection,
+      keypair,
+      ata_from.address,
+      ata_to.address,
+      keypair.publicKey,
+      1e6
+    );
+
+    console.log(`Your transfer txid: ${tx}`);
+
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+
+    // Get the token account of the toWallet address, and if it does not exist, create it
+
+    // Transfer the new token to the "toTokenAccount" we just created
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
